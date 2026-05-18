39798f2c239509870604a51a53abf4a051e624da
Kristika Sedai <ksedai@ncsu.edu>
2026-05-18 15:35:33 -0400

    Initial setup

---
+++ assistant-scheduler/src/main/java/com/summerproject2026/assistant_scheduler/AssistantSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.summerproject2026.assistant_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantSchedulerApplication.class, args);
+	}
+
+}
---
+++ assistant-scheduler/src/test/java/com/summerproject2026/assistant_scheduler/AssistantSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summerproject2026.assistant_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
